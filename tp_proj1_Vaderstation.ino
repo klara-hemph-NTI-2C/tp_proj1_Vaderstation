8bf6ad687d4bc540cf7cf1f470a842ef8a4eb2a1
klara-hemph-NTI-2C <klara.hemph@elev.ga.ntig.se>
2023-12-13 13:34:54 +0100

    Update tp_proj1_Vaderstation.ino

--- tp_proj1_Vaderstation.ino
+++ tp_proj1_Vaderstation.ino
@@ -1,128 +1,113 @@
+
 #include "U8glib.h"
 #include <DHT.h>
 #include <SGP30.h>
 
-#define pResistor A0 // photo resistor kopplad till analog 0 
-#define DHTPIN 2     // temp and air humidity connected to pin 2
-#define DHTTYPE DHT11   // DHT 11  (AM2302)
 
-DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
+#define DHTPIN 2              // Temp- och luftfuktighetssensor ansluten till pin 2
+#define DHTTYPE DHT11         // DHT 11 (AM2302)
 
-SGP30 SGP;
+DHT dht(DHTPIN, DHTTYPE);     // Initiera DHT-sensorn 
 
-// data read
-float hum;  //Stores humidity value
-float temp; //Stores temperature value
-float co2;
-float voc;
+SGP30 SGP;                   
 
+// Variabler för att lagra avläst data
+float hum;                   // Lagrar luftfuktighetsvärde
+float temp;                  // Lagrar temperaturvärde
+float co2;                   // Lagrar co2-värde
+float voc;                   // Lagrar voc-värde
 
-const int buttonPin = 4;  // the number of the pushbutton pin
-int buttonState = 0;  // variable for reading the pushbutton status
+const int buttonPin = 4;     // Pin för tryckknappen
+int buttonState = 0;         // Variabel för att läsa knappens status
 
-const int arraySize = 128;
-float dataArray[arraySize];
+const int arraySize = 128;   // Storlek på dataarrayen
+float dataArray[arraySize];   // Array för att lagra temperaturdata över tid
 
-int currentIndex = 0;
+int currentIndex = 0;        // Nuvarande index i datarrayen
 
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_DEV_0 | U8G_I2C_OPT_NO_ACK | U8G_I2C_OPT_FAST);  // Fast I2C / TWI
-
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_DEV_0 | U8G_I2C_OPT_NO_ACK | U8G_I2C_OPT_FAST); // Snabb I2C / TWI
 
 void setup(void) {
-  pinMode(pResistor, INPUT); //
   dht.begin();
   Serial.begin(9600);
   SGP.begin();
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT);  // Ange knappen som inmatning
 }
 
 void loop(void) {
-  // picture loop
-
+  // Mät gasvärden från SGP30
   SGP.measure(true);
 
   buttonState = digitalRead(buttonPin);
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    graf();
-  } else {
-    draw();
-  }
-  
 
+  // Kolla om knappen är nedtryckt. Om den är det, sätt buttonState till HIGH:
+  if (buttonState == HIGH) {
+    draw();    // Anropa funktionen för att rita information på displayen
+  } else {
+    graf();    // Anropa funktionen för att rita graf över tid
+  }
+
+  // Hämta aktuella värden för CO2, TVOC, luftfuktighet och temperatur
   co2 = SGP.getCO2();
   voc = SGP.getTVOC();
   hum = dht.readHumidity();
   temp = dht.readTemperature();
-  addToArray(temp);
+  
+  addToArray(temp);  // Lägg till temperaturvärde i arrayen
 
-  for (int i = 0; i < arraySize; i++)
-  {
+  // Skriv ut innehållet i arrayen
+  for (int i = 0; i < arraySize; i++) {
     Serial.println(dataArray[i]);
   }
 }
 
-
+// Funktion för att rita information på displayen
 void draw(void) {
-  // graphic commands to redraw the complete screen should be placed here
-
   u8g.firstPage();
   do {
-
     u8g.setFont(u8g_font_unifont);
 
-    u8g.drawStr( 15, 23, String(co2).c_str());
-    u8g.drawStr( 15, 9, String("co2:").c_str());
+    // Rita CO2-värde
+    u8g.drawStr(15, 23, String(co2).c_str());
+    u8g.drawStr(15, 9, String("co2:").c_str());
 
-    u8g.drawStr( 89, 23, String(voc).c_str());
-    u8g.drawStr( 89, 9, String("voc:").c_str());
+    // Rita TVOC-värde
+    u8g.drawStr(89, 23, String(voc).c_str());
+    u8g.drawStr(89, 9, String("voc:").c_str());
 
-    u8g.drawStr( 15, 63, String(hum).c_str());
-    u8g.drawStr( 15, 49, String("hum:").c_str());
+    // Rita luftfuktighet
+    u8g.drawStr(15, 63, String(hum).c_str());
+    u8g.drawStr(15, 49, String("hum:").c_str());
 
-    u8g.drawStr( 89, 63, String(temp).c_str());
-    u8g.drawStr( 89, 49, String("temp:").c_str());
+    // Rita temperatur
+    u8g.drawStr(89, 63, String(temp).c_str());
+    u8g.drawStr(89, 49, String("temp:").c_str());
 
-  } while ( u8g.nextPage() );
-
-
+  } while (u8g.nextPage());
 }
 
+// Funktion för att rita en graf över tid
 void graf(void) {
-  //jag älksar dig klara / Nicholas
-
   u8g.firstPage();
   do {
-
     u8g.setFont(u8g_font_unifont);
 
-    for (int i = 0; i < arraySize; i++)
-    {
+    // Rita datapunkter i grafen
+    for (int i = 0; i < arraySize; i++) {
       if (i == 0) {
         u8g.drawPixel(i, (63 - dataArray[i]));
       } else {
-
-        int y1 = 32+(dataArray[i]-20);
-        int y2 = 32+(dataArray[i-1]-20);
-        u8g.drawLine((i-1), ((((20 - dataArray[i-1]))*3)+40), i, ((((20 - dataArray[i])))*3)+40);
+        int y1 = 32 + (dataArray[i] - 20);
+        int y2 = 32 + (dataArray[i - 1] - 20);
+        u8g.drawLine((i - 1), ((((20 - dataArray[i - 1])) * 3) + 40), i, ((((20 - dataArray[i]))) * 3) + 40);
       }
-      // max 30 min 10
-
-      // 25* man får 4 som y, man har 30 pixlar upp, 
     }
 
-
-  } while ( u8g.nextPage() );
-
-
-
+  } while (u8g.nextPage());
 }
 
-void addToArray(float a) {
-  dataArray[currentIndex] = a;
-
+// Funktion för att lägga till temperaturvärde i arrayen och rotera om arrayen är full
+void addToArray(float value) {
+  dataArray[currentIndex] = value;
   currentIndex = (currentIndex + 1) % arraySize;
 }
-
-
-/////////////////////////////////////////////////////////
